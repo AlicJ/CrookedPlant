be0c7f8c760e386a7f5d5050596ec6c59c0bcb22
Tarun Khanna <tennispro1213@gmail.com>
2018-01-13 14:11:35 -0500

    Add simple chartjs view for allData

--- angularjs/my-app/src/app/calendar.component.ts
+++ angularjs/my-app/src/app/calendar.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { CalendarEvent, CalendarMonthViewDay } from 'angular-calendar';
 import { DayViewHour } from 'calendar-utils';
 import { colors } from './demo-utils/colors';
+import * as Chart from 'chart.js';
+Chart.defaults.global.defaultFontColor = 'black';
 declare var jquery:any;
 declare var $ :any;
 
@@ -12,12 +14,17 @@ declare var $ :any;
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./app.component.css']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements OnInit, AfterViewInit {
 
-  allData = JSON.parse(localStorage.getItem('allData'));
+  // allData = JSON.parse(localStorage.getItem('allData'));
   allDate = []
   allDays = []
   selectedDay = {};
+  
+  allData : any;
+  canvas: any;
+  ctx: any;
+  chartdata : any;
 
   onSelect(day: Object): void {
     console.log("day selected", this);
@@ -43,6 +50,8 @@ export class CalendarComponent implements OnInit {
   }
 
   ngOnInit(self = this): void {
+    self.allData = JSON.parse(localStorage.getItem('allData'));
+    
     if (!self.allData) throw("Initialize localhost by runnning CrookedPlant/allData.txt");
     
   	$.each(self.allData, function(key, value){
@@ -66,10 +75,46 @@ export class CalendarComponent implements OnInit {
           meta: e[keyVal]
         }
       );
-      console.log(e,colors[e[keyVal]], e[keyVal]);
+      // console.log(e,colors[e[keyVal]], e[keyVal]);
     })
     self.events = self.events.concat(tempEventsArr);
-    console.log(self.allData);
-    console.log("Events", self.events);
+    // console.log(self.allData);
+    // console.log("Events", self.events);
+  }
+
+
+  ngAfterViewInit(self = this): void {
+    self.chartdata = {};
+    var tempAllData = Array(self.allData)[0];
+    tempAllData.forEach((v,i,k) => {self.chartdata[Object.values(v)[0].general]=0});
+    tempAllData.forEach((v,i,k) => {self.chartdata[Object.values(v)[0].general]+=1});
+
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+
+    let myChart = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+          labels: Object.keys(self.chartdata),
+          datasets: [{
+              label: 'Count',
+              data: Object.values(self.chartdata),
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'red'
+              ],
+              borderWidth: 10
+          }]
+      },
+      options: {
+        responsive: false,
+        title: {
+          text: "All Emotions",
+          display: true
+        }
+      }
+    });
   }
 }
